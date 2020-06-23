de26602be9ba46e29ca9ed379a21e77cc354537f
dkamianskii <kamianskii_dmitrii@mail.ru>
2020-06-23 10:34:20 +0300

    Added recognition and matching face in upload component

--- src/app/components/upload.js
+++ src/app/components/upload.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Content from "./content";
+import * as faceAPI from "../faceapi";
+import * as faceapi from 'face-api.js';
+import {labels} from "../../resourсes/labels";
 
-import face001 from '../../resourсes/face001.jpg';
-import face002 from '../../resourсes/face002.jpg';
-import face003 from '../../resourсes/face003.jpg';
-import face004 from '../../resourсes/face004.jpg';
+// import face001 from '../../resourсes/face001.jpg';
+// import face002 from '../../resourсes/face002.jpg';
+// import face003 from '../../resourсes/face003.jpg';
+// import face004 from '../../resourсes/face004.jpg';
 
+// Loading imagies and descriptors of base pics
+const JSON_PROFILE = require('../../resourсes/labeledFaceDescriptors.json');
+const imagesDict = {}
+labels.forEach(label => {imagesDict[label] = require(`../../resourсes/labeled_images/${label}/1.jpg`)})
+//
 
 class Upload extends React.Component {
     static fileTypes = ["image/jpeg", "image/pjpeg", "image/jpg", "image/png"];
@@ -13,15 +21,18 @@ class Upload extends React.Component {
 
     constructor(props) {
         super(props);
-
+        //Loadings
+        this.state = {faceMatcher: null };
+        this.loadingDependences();
+        //
         this.handleChange = this.handleChange.bind(this);
         this.fileInput = React.createRef();
     }
 
     // [a, b]
-    getRandomInt(a, b) {
-        return Math.floor(Math.random() * (b - a + 1) + a);
-    }
+    // getRandomInt(a, b) {
+    //     return Math.floor(Math.random() * (b - a + 1) + a);
+    // }
 
     move(target, T, frames) {
         let dt = T / frames;
@@ -47,7 +58,7 @@ class Upload extends React.Component {
         }
     }
 
-    handleChange(event) {
+    async handleChange(event) {
         event.preventDefault();
 
         if (this.fileInput.current.files.length !== 1) {
@@ -64,18 +75,47 @@ class Upload extends React.Component {
 
             document.getElementById("card").className = "card border-success";
             document.getElementById("cardHeader").className = "card-header bg-success";
+            // Face detection
+            const image = await faceapi.bufferToImage(file);
+            const detections = await faceAPI.getDetections(image);
+            console.log(detections);
+            let result = null;
+            if(detections.length === 0){
+                console.log("there is no face on a picture");
+            }else{
+                if(detections.length === 1){
+                    result = this.state.faceMatcher.findBestMatch(detections[0].descriptor);
+                    console.log(result._label,result._distance);
+                }else{
+                    result = detections.map(d => this.state.faceMatcher.findBestMatch(d.descriptor));
+                    // result.forEach(res => console.log(res.toString));
+                    console.log(result);
+                }
+            }
+            //
             input.src = URL.createObjectURL(file);
             input.hidden = false;
             document.getElementById("cardHeader").textContent = Content.successUpload();
 
-            // рандом
-            let output = document.getElementById("output");
-            let src = [face001, face002, face003, face004];
+            // рандом            
+            // let src = [face001, face002, face003, face004];
+            // let rnd = this.getRandomInt(0, 100);
 
-            let rnd = this.getRandomInt(0, 100);
+            let output = document.getElementById("output");
+            //Face matching
+            let src = null;
+            if(detections.length === 1){
+                // match.textContent = Content.match() + result._label + " on " + Math.round(result._distance*100) + "%";
+                src = imagesDict[result._label];
+            }else{
+                // match.textContent = Content.match() + result[0]._label + " on " + Math.round(result[0]._distance*100) + "%";
+                src = imagesDict[result[0]._label];
+            }
+            output.src = src;
+            //
             let T = 500;
-            this.move(rnd, T, T);
-            output.src = src[this.getRandomInt(0, 3)];
+            this.move(result._distance*100, T, T);
+            // output.src = src[this.getRandomInt(0, 3)];
             output.hidden = false;
 
             this.scrollToAnchor("input");
